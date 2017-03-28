6737f1ddbac1db99e4edbc6e557de57744b339a3
PMichalik <paul.michalik@harman.com>
2017-03-28 14:30:43 +0200

    Prepare for fetching from ros repositories

--- vagrant/create_environment.sh
+++ vagrant/create_environment.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 apt-get update && apt-get install -y \
 	ros-indigo-desktop-full \
@@ -28,3 +32,4 @@ apt-get update && apt-get install -y \
 	libpcap-dev \
 	gksu \
 	libgl1-mesa-dev
+	
