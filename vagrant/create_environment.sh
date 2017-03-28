1625beb3f2fe0ba12c337024b8aefbffdc2c4a0e
PMichalik <paul.michalik@harman.com>
2017-03-28 14:55:01 +0200

    Complete script which runs on a clean ubuntu-14.04.5-desktop-amd64 in virtualbox.

--- vagrant/create_environment.sh
+++ vagrant/create_environment.sh
@@ -4,7 +4,9 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
-apt-get update && apt-get install -y \
+apt-get update
+
+apt-get install -y \
 	ros-indigo-desktop-full \
 	ros-indigo-nmea-msgs \
 	ros-indigo-nmea-navsat-driver \
@@ -12,7 +14,7 @@ apt-get update && apt-get install -y \
 	ros-indigo-jsk-visualization \
 	ros-indigo-grid-map
 
-apt-get update && apt-get install -y \
+apt-get install -y \
 	ros-indigo-controller-manager \
 	ros-indigo-ros-control \
 	ros-indigo-ros-controllers \
@@ -22,7 +24,7 @@ apt-get update && apt-get install -y \
 	ros-indigo-joystick-drivers \
 	ros-indigo-novatel-span-driver
 
-apt-get update && apt-get install -y \
+apt-get install -y \
 	libnlopt-dev \
 	freeglut3-dev \
 	qtbase5-dev \
