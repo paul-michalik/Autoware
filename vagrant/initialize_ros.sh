988d715db7e7d24722c526e655a8e79f0e028168
PMichalik <paul.michalik@harman.com>
2017-03-31 11:34:16 +0200

    Added initialization scripts.

---
+++ vagrant/initialize_ros.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bash
+
+# Run this first time after installing ros. Do not run the script with sudo!
+sudo rosdep init
+
+rosdep update
+
---
+++ vagrant/initialize_ros_shell.sh
@@ -0,0 +1,4 @@
+﻿#!/usr/bin/env bash
+
+# Run this in each shell where ros is used:
+source /opt/ros/indigo/setup.sh
