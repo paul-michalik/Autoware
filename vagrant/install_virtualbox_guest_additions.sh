6caf8177faf8b853931730fdb9d5987acca4820e
PMichalik <paul.michalik@harman.com>
2017-03-31 12:36:13 +0200

    Modified   vagrant/create_environment.sh Add        vagrant/install_virtualbox_guest_additions.sh

---
+++ vagrant/install_virtualbox_guest_additions.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+
+# Installs virtualbox guest additions on Ubuntu-server. You'll need to "insert the CD" manually. Run with sudo!
+apt-get update
+
+apt-get install -y dkms build-essential
+
+echo "reboot and mount the folders manually!"
+ 
